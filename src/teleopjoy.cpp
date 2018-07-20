aaa2e0103bd9e9dd5d0c8b98860a95dea7e6d4e8
Guy Corbaz <guy@corbaz.org>
2018-07-20 09:59:26 +0200

    Added licences header to file

--- src/teleopjoy.cpp
+++ src/teleopjoy.cpp
@@ -1,12 +1,25 @@
-/*!
- * \brief Teleoperation node for Gribot
- *
- * The goal of this noe is to translate
- * teleoperation commands comming from a
- * joystick and send them to Gribot
- * in order to allow to pilot Gribot
- * with a joystick.
+/*
+Joystick teleop node for Gribot robot
+
+Copyright (C) 2018  Guy Corbaz
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+This node transforms tele operating commands comming from a joystick
+into cmd_vel command
 */
+
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
@@ -16,7 +29,7 @@
 using namespace std;
 
 
-/*! */
+/* Handling tele operation commands */
 
 class TeleopJoy
 {
@@ -36,9 +49,9 @@ class TeleopJoy
 
 TeleopJoy::TeleopJoy()
 {
-   n.param("axis_linear", i_velLinear, i_velLinear);	// From parameter server 
-   n.param("axix_angular", i_velAngular, i_velAngular); // From parameter server 
-   pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1); // Published 
+   n.param("axis_linear", i_velLinear, i_velLinear);		// From parameter server
+   n.param("axix_angular", i_velAngular, i_velAngular); 	// From parameter server
+   pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);	// Published
    sub = n.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopJoy::callBack, this); // Subscribed 
 }
 
@@ -47,10 +60,10 @@ TeleopJoy::TeleopJoy()
 
 void TeleopJoy::callBack(const sensor_msgs::Joy::ConstPtr& joy)
 {
-   geometry_msgs::Twist vel;			// Used to publish data 
+   geometry_msgs::Twist vel;					// Used to publish data
    vel.angular.z = -2*joy->axes[i_velAngular];  // Factor 2 required to increase angular speed, négative sign see below 
    vel.linear.x = -1*joy->axes[i_velLinear];    // Negative sign required to correct joytisk inversion
-   pub.publish(vel);				// The topic is published 
+   pub.publish(vel);							// The topic is published
 }
 
 
